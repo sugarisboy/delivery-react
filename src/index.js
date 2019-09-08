6db63712f138801c12d75b81a906178214ebcd09
Konstantin Matrokhin <matrohin@scrile.com>
2019-09-08 13:38:53 +0700

    added iml file

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './containers/App'
 import {BrowserRouter} from 'react-router-dom'
-import {Provider} from "react-redux";
-import {store} from "./store";
+import {Provider} from "react-redux"
+import {store} from "./store"
 
 
 ReactDOM.render((
