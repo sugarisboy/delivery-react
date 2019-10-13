83144ef5e7a57f6f6b9d8f6efcf3704a25d3a512
remillary <konstantin@muskrat.dev>
2019-10-13 15:28:40 +0700

    checkout page

--- src/containers/CheckoutPage/index.jsx
+++ src/containers/CheckoutPage/index.jsx
@@ -1,23 +1,91 @@
-import React from 'react'
+import React, { Component, Fragment } from 'react'
 import { connect } from 'react-redux'
+import { get } from '../../service/api'
 
-class CheckoutPage extends React.Component {
+class CheckoutPage extends Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            shop: null,
+            checkoutItems: [],
+            totalPrice: 0
+        }
+    }
+
+    componentDidMount() {
+        this.forceUpdate()
+    }
+
+    async componentDidUpdate(prevProps) {
+        const {match, cart} = this.props
+
+        if (!match) {
+            return
+        }
+
+        const shopId = match.params.id
+        const prevShopId = prevProps.match.params.id
+
+        if (shopId !== prevShopId || this.state.shop === null) {
+            const resp = await get(`/shop/${shopId}`)
+            const shop = resp.data
+            const shopCart = cart[shopId]
+
+            let totalPrice = 0
+            const checkoutItems = []
+            for (let item in shopCart) {
+                if (shopCart.hasOwnProperty(item) && shopCart[item].hasOwnProperty('count')) {
+                    const resp = await get(`/product/${item}`)
+                    const productData = resp.data
+                    const count = shopCart[item].count
+
+                    totalPrice += (productData.price * count)
+
+                    checkoutItems.push({
+                        ...productData,
+                        count
+                    })
+                }
+            }
+
+            console.log(checkoutItems)
+
+            this.setState({ shop, checkoutItems, totalPrice })
+        }
+    }
+
     render() {
-        return (
-            <React.Fragment>
-                <h1>Checkout</h1>
+        const { shop, checkoutItems, totalPrice } = this.state
 
-            </React.Fragment>
+        return (
+            <Fragment>
+                {shop && (
+                    <Fragment>
+                        <h1>Checkout {shop.name}</h1>
+                        <div>
+                            {checkoutItems.map(item => (
+                                <div key={item.id}>
+                                    <span>ID {item.id}</span>,
+                                    <span>Title {item.title}</span>,
+                                    <span>Description {item.description}</span>,
+                                    <span>Price ${item.price}</span>,
+                                    <span>Count {item.count}</span>
+                                </div>
+                            ))}
+                        </div>
+                        <h2>Total Price: ${totalPrice}</h2>
+                    </Fragment>
+                )}
+            </Fragment>
         )
 
     }
 }
 
-const mapDispatchToProps = {
-}
-
 const mapStateToProps = state => ({
-    openedShopId: state.shop.openedShopId
+    cart: state.cart
 })
 
-export default connect()(CheckoutPage)
+export default connect(mapStateToProps)(CheckoutPage)
