51fd61ea1852189292a85aad574b57b294f656e5
remillary <konstantin@muskrat.dev>
2019-10-13 21:57:27 +0700

    searching WIP

--- src/containers/Search/index.jsx
+++ src/containers/Search/index.jsx
@@ -1,14 +1,24 @@
 import React from 'react'
 import './style.css'
+import { get } from '../../service/api'
 
 export default class Search extends React.Component {
+
+    async onChange(e) {
+        const {value} = e.target
+        const res = await get(`/map/ac/${value}`)
+        const suggestions = res.data
+        console.log(suggestions)
+    }
 
     render() {
         return (
             <div className="search">
                 <input type="text"
                        className="search__input"
-                       placeholder="Add your address"/>
+                       placeholder="Add your address"
+                       onChange={this.onChange}
+                />
 
                 <button className="search__button">Search</button>
             </div>
