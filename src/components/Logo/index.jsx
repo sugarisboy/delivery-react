cae62949c288e2ed8af67c9aa9ae6ef9e694fdad
remillary <konstantin@muskrat.dev>
2019-10-20 18:02:24 +0700

    styled components

--- src/components/Logo/index.jsx
+++ src/components/Logo/index.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import './style.css'
 import {Link} from 'react-router-dom'
 import styled from 'styled-components'
 import img from './small-logo.png'
--- src/containers/Header/index.jsx
+++ src/containers/Header/index.jsx
@@ -2,18 +2,31 @@ import React from 'react'
 import Logo from '../../components/Logo'
 import Navigation from '../Navigation'
 import CartButton from '../CartButton'
-import './style.css'
 import { connect } from 'react-redux'
+import styled from 'styled-components'
+
+const HeaderStyled = styled.header`
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    margin-top: 22px;
+    margin-bottom: 15px;
+    
+    @media (max-width: 800px) {
+        flex-direction: column;
+    }
+`
 
 class Header extends React.Component {
 
     render() {
         return (
-            <header className="header">
+            <HeaderStyled>
                 <Logo/>
                 <Navigation/>
                 {this.props.openedShopId > 0 && <CartButton/>}
-            </header>
+            </HeaderStyled>
         )
     }
 }
