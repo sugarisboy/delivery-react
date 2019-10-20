93be2df3ce36bc194c6c41aa0ee6f9687c164568
remillary <konstantin@muskrat.dev>
2019-10-20 13:48:44 +0700

    search

---
+++ src/actions/user.js
@@ -0,0 +1,10 @@
+import { SET_ADDRESS } from './actions-types'
+
+export function setAddress(address) {
+    return dispatch => {
+        dispatch({
+            type: SET_ADDRESS,
+            payload: address
+        })
+    }
+}
--- src/containers/Search/index.jsx
+++ src/containers/Search/index.jsx
@@ -1,23 +1,61 @@
 import React from 'react'
-import './style.css'
+import AsyncSelect from 'react-select/async'
+import './style.scss'
 import { get } from '../../service/api'
+import { connect } from 'react-redux'
+import { setAddress } from '../../actions/user'
 
-export default class Search extends React.Component {
+class Search extends React.Component {
 
-    async onChange(e) {
-        const {value} = e.target
-        const res = await get(`/map/ac/${value}`)
-        const suggestions = res.data
-        console.log(suggestions)
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            suggestions: []
+        }
+    }
+
+    async onChange(val) {
+        console.log(val)
+        // const res = await get(`/map/ac/${value}`)
+        // const {suggestions} = res.data
+        //
+        // this.setState({
+        //     suggestions: suggestions.filter(s => (s.address.street && s.address.houseNumber))
+        // })
+    }
+
+    loadOptions = async(val) => {
+        const res = await get(`/map/ac/${val}`)
+        const {suggestions} = res.data
+        const filtered = suggestions.filter(s => (s.address.street && s.address.houseNumber))
+        return suggestions.map(s => ({label: s.label}))
     }
 
     render() {
         return (
             <div className="search">
-                <input type="text"
-                       className="search__input"
-                       placeholder="Add your address"
-                       onChange={this.onChange}
+                <AsyncSelect type="text"
+                             cacheOptions
+                             className="search__input"
+                             placeholder="Add your address"
+                             loadOptions={this.loadOptions}
+                             onInputChange={this.onChange.bind(this)}
+                             styles={{
+                                 control: styles => ({
+                                     ...styles,
+                                     padding: 12
+                                 }),
+                                 dropdownIndicator: () => ({
+                                     display: 'none'
+                                 }),
+                                 input: styles => ({
+                                     ...styles
+                                 }),
+                                 indicatorsContainer: () => ({
+                                     display: 'none'
+                                 })
+                             }}
                 />
 
                 <button className="search__button">Search</button>
@@ -26,3 +64,9 @@ export default class Search extends React.Component {
     }
 
 }
+
+const mapDispatchToProps = dispatch => ({
+    setAddress: address => dispatch(setAddress(address))
+})
+
+export default connect(null, mapDispatchToProps)(Search)
