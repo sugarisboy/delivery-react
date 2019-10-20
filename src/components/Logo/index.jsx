35b17ca518e548a2981930f51f9298fc7b9e6d15
remillary <konstantin@muskrat.dev>
2019-10-20 17:59:46 +0700

    styled components for logo

--- src/components/Logo/index.jsx
+++ src/components/Logo/index.jsx
@@ -1,13 +1,31 @@
 import React from 'react'
 import './style.css'
 import {Link} from 'react-router-dom'
+import styled from 'styled-components'
+import img from './small-logo.png'
+
+const StyledLink = styled(Link)`
+    background-image: url(${img});
+    background-color: transparent;
+    background-repeat: no-repeat;
+    width: 133px;
+    height: 28px;
+
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    appearance: none;
+    
+    border: none;
+    
+    &:hover {
+        cursor: pointer;
+    }
+`
 
 export default class Logo extends React.PureComponent {
-
     render() {
         return (
-            <Link to="/" className="logo"/>
+            <StyledLink to="/"/>
         )
     }
-
 }
