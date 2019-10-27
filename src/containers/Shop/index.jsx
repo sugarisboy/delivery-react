d641c210868a61011912eeed0fcfb4cdd0613068
remillary <konstantin@muskrat.dev>
2019-10-27 21:09:07 +0700

    preview

--- src/containers/Shop/index.jsx
+++ src/containers/Shop/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './style.scss'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import { API_URL } from '../../service/api'
 
 export default class Shop extends React.Component {
 
@@ -10,9 +11,15 @@ export default class Shop extends React.Component {
     }
 
     render() {
+        const { id } = this.info
+
         return (
-            <Link to={`/shop/${this.info.id}`} className="shop">
-                <div className="shop__logo"/>
+            <Link to={`/shop/${id}`} className="shop">
+                <div className="shop__logo" style={{
+                    background: `url(${API_URL}/img/shop/${id}.jpg) #000`,
+                    backgroundPosition: 'center',
+                    backgroundSize: 'cover',
+                }}/>
                 <div className="shop__info-block">
                     <div className="shop__name">
                         {this.info.name}
