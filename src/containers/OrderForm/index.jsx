d87ddeabdd558175d47104bf82401647f20a18ae
remillary <konstantin@muskrat.dev>
2019-10-27 14:03:20 +0700

    order form WIP

--- src/containers/OrderForm/index.jsx
+++ src/containers/OrderForm/index.jsx
@@ -3,21 +3,124 @@ import QwikInput from '../../components/QwikInput'
 import QwikTextarea from '../../components/QwikTextarea'
 import './style.scss'
 import { connect } from 'react-redux'
+import { get, post } from '../../service/api'
 
 class OrderForm extends React.Component {
 
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            userData: {
+                name: '',
+                phone: '',
+                email: '',
+                comment: ''
+            }
+        }
+    }
+
+    async componentDidMount() {
+        const {id} = this.props.auth.userData
+        if (!id) {
+            return
+        }
+
+        try {
+            const resp = await get(`/user/${id}`)
+            const userData = resp.data
+            this.setState({userData})
+        } catch (e) {
+
+        }
+    }
+
+    normalizeProducts(cart, shopId) {
+        const shopCart = cart[shopId]
+        const productsArray = []
+
+        if (shopCart) {
+            const keys = Object.keys(shopCart)
+
+            if (keys.length > 0) {
+                for (let key of keys) {
+                    if (shopCart.hasOwnProperty(key)) {
+                        const item = shopCart[key]
+                        if (item.hasOwnProperty('count')) {
+                            const count = item.count
+                            if (count > 0) {
+                                productsArray.push({
+                                    productId: +key,
+                                    count: count
+                                })
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return productsArray
+    }
+
+    onSubmit = async e => {
+        const {shopId, auth, cart} = this.props
+
+        e.preventDefault()
+
+        const products = this.normalizeProducts(cart, shopId)
+        console.log(products)
+
+        const resp = await post('/order/create', {
+            ...this.state.userData,
+            products,
+            shopId: shopId,
+            userId: auth.userData.id || undefined,
+        })
+        console.log(resp)
+    }
+
+    onChange = e => {
+        const {name, value} = e.target
+        const newUserData = this.state.userData
+
+        newUserData[name] = value
+        this.setState({
+            userData: newUserData
+        })
+    }
+
     render() {
         const {user} = this.props
+        const {userData} = this.state
 
         return (
             <div>
-                <form className="order-form">
-                    <QwikInput name="address" title="Address" type="text" defaultValue={user.location.label}/>
-                    <QwikInput name="firstName" title="First Name" type="text"/>
-                    <QwikInput name="lastName" title="Last Name" type="text"/>
-                    <QwikInput name="phone" title="Phone" type="tel"/>
-                    <QwikInput name="email" title="Email" type="email"/>
-                    <QwikTextarea name="comment" title="Comment" cols={30} rows={10}/>
+                <form className="order-form" onSubmit={this.onSubmit}>
+                    <QwikInput name="address"
+                               title="Address"
+                               type="text"
+                               defaultValue={user.location.label}
+                               // required
+                               onChange={this.onChange}/>
+                    <QwikInput name="name"
+                               title="Full Name"
+                               type="text"
+                               value={userData.name}
+                               // required
+                               onChange={this.onChange}/>
+                    <QwikInput name="phone"
+                               title="Phone"
+                               type="tel"
+                               value={userData.phone}
+                               // required
+                               onChange={this.onChange}/>
+                    <QwikInput name="email"
+                               title="Email"
+                               type="email"
+                               value={userData.email}
+                               onChange={this.onChange}/>
+                    <QwikTextarea name="comment" title="Comment" cols={30} rows={10} onChange={this.onChange}/>
 
                     <button type="submit" className="button">Next</button>
                 </form>
@@ -28,7 +131,8 @@ class OrderForm extends React.Component {
 
 const mapStateToProps = state => ({
     user: state.user,
-    auth: state.auth
+    auth: state.auth,
+    cart: state.cart
 })
 
 export default connect(mapStateToProps)(OrderForm)
